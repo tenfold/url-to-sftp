15cddcacbfa72dccdd72f0834c108ddeab049d54
Patrick Hogan <patrick@callinize.com>
2016-03-09 03:54:36 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 var _ = require('lodash');
 var request = require('request');
 var Client = require('scp2').Client;
+var progress = require('progress-stream');
 
 module.exports.send = function send(link, scpOpts, destination, callback) {
     if (!link) throw new Error('Missing required parameter.  Requires http link - e.g. http://domain.com/file.txt');
     if (!scpOpts) throw new Error('Missing required parameter.  Requires sftp server options.  We need to know where to send the file.');
     if (!destination) throw new Error('Missing required parameter.  Requires destination on the SFTP server - e.g. /files/test.txt');
     if (!callback) callback = _.noop;
-    request.get(link, function (err, resp, body) {
-        if (err) return callback(err);
-        if (!body) return callback('NO_BODY_RETURNED');
+    request.head(link, function (err, resp, body) {
+        if(err) return callback(err);
+        var contentLength = resp.headers['content-length'];
         var client = new Client(scpOpts);
-        client.write({
-            destination: destination,
-            content: new Buffer(body, scpOpts.contentType)
+        //todo make the time setting configurable
+        var progressStr = progress({time: 5000, length: contentLength}, function (progress) {
+            //todo allow a custom logger from opts
+            console.log(destination, 'transferred:', progress.transferred, 'remaining:', progress.remaining);
+        });
+
+        client.writeStream(destination, function getWriteableStream(err, writeableStream) {
+            if(err) return callback(err);
+            request(link).pipe(progressStr).pipe(writeableStream);
         }, callback);
-    });
+    })
 }
