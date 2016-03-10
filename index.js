0f620dd6c8165c333581b17e42bc323f7c3d237c
Kessiler A. S. Rodrigues <keessiler@hotmail.com>
2016-03-10 15:00:54 -0600

    add opts as parameter

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ var request = require('request');
 var Client = require('scp2').Client;
 var progress = require('progress-stream');
 
-module.exports.send = function send(link, scpOpts, destination, callback) {
+module.exports.send = function send(link, scpOpts, destination, opts, callback) {
     if (!link) throw new Error('Missing required parameter.  Requires http link - e.g. http://domain.com/file.txt');
     if (!scpOpts) throw new Error('Missing required parameter.  Requires sftp server options.  We need to know where to send the file.');
     if (!destination) throw new Error('Missing required parameter.  Requires destination on the SFTP server - e.g. /files/test.txt');
-    if (!callback) callback = _.noop;
-    request.head(link, function (err, resp, body) {
-        if(err) return callback(err);
+    callback = callback || _.noop;
+    _.defaults(opts, {url: '', jar: ''});
+    opts.url = link;
+    request.head(opts, function (err, resp) {
+        if (err) return callback(err);
         var contentLength = resp.headers['content-length'];
         var client = new Client(scpOpts);
         //todo make the time setting configurable
@@ -19,8 +21,8 @@ module.exports.send = function send(link, scpOpts, destination, callback) {
         });
 
         client.writeStream(destination, function getWriteableStream(err, writeableStream) {
-            if(err) return callback(err);
-            request(link).pipe(progressStr).pipe(writeableStream);
+            if (err) return callback(err);
+            request(opts).pipe(progressStr).pipe(writeableStream);
         }, callback);
     })
-}
+};
